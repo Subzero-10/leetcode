5b83fcec21208b83dd6192112e98292163894e62
shizheng(shiyanshi) <shizheng961111@gmail.com>
2020-09-09 17:09:19 +0800

    no message

---
+++ .history/143.reorder-list_20200909170139.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=143 lang=cpp
+ *
+ * [143] Reorder List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        stack<ListNode*> s;
+        ListNode* root = head;
+        while (root)
+        {
+            s.push(root);
+            root = root->next;
+        }
+        root = head;
+        while (root->next && (root->next != s.top()))
+        {
+            ListNode* tem = root->next;
+            root->next = s.top();
+            s.top()->next = tem;
+            s.pop();
+            root = tem;
+            printf("%d",root->next->val);
+        }
+    }
+};
+// @lc code=end
+
